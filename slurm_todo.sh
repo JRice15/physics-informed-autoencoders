9ddf064dfaf9cdf66ab2e63e117ccfe85778a75b
Julian Rice <julianrice@dotcomjungle.com>
2020-07-27 15:20:29 -0700

    slurm task files

---
+++ slurm_todo.sh
@@ -0,0 +1,12 @@
+
+python3 train_autoencoder.py --model koopman --no-basemap --name k --convol --seed 0 --filters 8 16 32 --activation relu
+
+for s in {1..3}; do
+	python3 train_autoencoder.py --model koopman --no-basemap --name b --convol --seed "$s" --filters 8 16 32 --activation relu --bwd-steps 0 
+	python3 train_autoencoder.py --model koopman --no-basemap --name k --convol --seed "$s" --filters 8 16 32 --activation relu
+	python3 train_autoencoder.py --model koopman --no-basemap --name b --convol --seed "$s" --filters 8 16 32 --bwd-steps 0 
+	python3 train_autoencoder.py --model koopman --no-basemap --name k --convol --seed "$s" --filters 8 16 32
+	python3 train_autoencoder.py --model koopman --no-basemap --name b --convol --seed "$s" --filters 16 32 64 --bwd-steps 0 
+	python3 train_autoencoder.py --model koopman --no-basemap --name k --convol --seed "$s" --filters 16 32 64
+done
+
