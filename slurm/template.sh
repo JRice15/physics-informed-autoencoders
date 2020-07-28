67a32f5485898449928f6e431a6a8561dbf9c566
Julian R Rice <rice566@marianas.pnl.gov>
2020-07-28 12:00:16 -0700

    correct package versioning for template

--- slurm/template.sh
+++ slurm/template.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 #SBATCH -A br20_rice566
 #SBATCH -J rice_pia
-#SBATCH -t 2:00:00
+#SBATCH -t 0:20:00
 #SBATCH -p all
 #SBATCH -N 1
-#SBATCH --gpus:1
+#SBATCH --gres=gpu:1
 #SBATCH -o /qfs/people/rice566/physics-informed-autoencoders/slurm/logs/{logname}.out
 #SBATCH -e /qfs/people/rice566/physics-informed-autoencoders/slurm/logs/{logname}.err
 
-source /etc/profile.d/modules.sh
-source /qfs/people/rice566/tf_gpu/bin/activate
-
 module purge
-module load cuda/9.0.176
+module load python/3.6.6
+module load cuda/10.0.130
+
+source /etc/profile.d/modules.sh
+source /qfs/people/rice566/tf_gpu2/bin/activate
 
 ulimit -s unlimited
 
 cd /qfs/people/rice566/physics-informed-autoencoders
 
 # Tasks Go Below. Leave some newlines to be safe
+
 
