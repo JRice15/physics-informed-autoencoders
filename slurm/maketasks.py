c8c013c1c9d327ed6a3ccdb7e49787f4182198dd
Julian Rice <julianrice@dotcomjungle.com>
2020-07-27 17:24:09 -0700

    slurm formatting

--- slurm/maketasks.py
+++ slurm/maketasks.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import string
 
 parser = argparse.ArgumentParser()
 parser.add_argument("name")
@@ -20,29 +21,37 @@ with open(args.file, "r") as f:
     test_lines = f.readlines()
 
 
-def do_writing(lines, index=None):
+def do_writing(template_lines, test_lines, index=None):
     """
     write a task file
     """
-    filename = args.name + "_task"
+    write_name = args.name + "_task"
     if index is not None:
-        filename += "_" + str(index)
-    filename += ".slurm"
+        write_name += "_" + str(index)
+
+    is_fmt = lambda line: len(tuple(string.Formatter().parse(line))) > 1
+    new_template_lines = [i.format(logname=write_name) if is_fmt(i) else i for i in template_lines]
+
+    filename = write_name + ".slurm"
+
     task_file = os.path.join(slurmdir, "tasks", filename)
     if os.path.exists(task_file):
-        prompt = "a task with name '{}' exists. overwrite? [y/n]: ".format(filename)
+        prompt = "A task with name '{}' exists. Overwrite? [y/n]: ".format(filename)
         if input(prompt).strip().lower() not in ("y", "yes"):
+            print("skipping...")
             return
-    full_lines = template_lines + test_lines
+        print("overwriting...")
+
+    full_lines = new_template_lines + test_lines
     with open(task_file, "w") as f:
         f.writelines(full_lines)
 
 
 if args.seperate:
+    test_lines = [i for i in test_lines if i.strip() != ""]
     for i in range(len(test_lines)):
         # write non-empty lines as their own task
-        if test_lines[i].strip() != "":
-            do_writing([test_lines[i]], i)
+        do_writing(template_lines, [test_lines[i]], i)
 else:
     # write all lines to a task
-    do_writing(test_lines)
+    do_writing(template_lines, test_lines)
